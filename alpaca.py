cb5b493be7e6f3b82fe1beb33df455a907c4931e
Mateusz <mati.p66@gmail.com>
2023-03-22 22:49:00 +0100

    update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -1,7 +1,8 @@
+import os
 from CustomLLM import AlpacaLLM
 
-llm = AlpacaLLM(model_path="./ggml-alpaca-7b-native-q4.bin")
+model_path = os.path.join(".", "ggml-alpaca-7b-native-q4.bin")
+llm = AlpacaLLM(model_path=model_path)
 
 output = llm("Hello, how are you?")
 print(output)
-
